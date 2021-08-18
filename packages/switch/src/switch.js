b69aae1fff1f02fe344e9f8d209b9d5c2e81230d
Josh Crawford <joshua@sgroup.com.au>
2021-08-18 14:51:26 +1000

    Add switch

---
+++ packages/switch/src/switch.js
@@ -0,0 +1,16 @@
+import * as UI from '@headlessui/vue';
+// export { Switch, SwitchDescription, SwitchGroup, SwitchLabel } from '@headlessui/vue';
+//
+
+export const Switch = {
+    extends: UI.Switch,
+};
+export const SwitchDescription = {
+
+};
+export const SwitchGroup = {
+
+};
+export const SwitchLabel = {
+
+};
